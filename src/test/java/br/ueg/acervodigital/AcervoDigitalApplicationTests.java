0bd06e4df2b9a1384eb76ef8876d7f0575664789
Erik Mota <seriikmota.dev@gmail.com>
2024-08-28 08:32:38 -0300

    repository initialization

---
+++ src/test/java/br/ueg/acervodigital/AcervoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.acervodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
