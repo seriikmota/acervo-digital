0b32b45e641c1ce25b2ad2e0074264f1cf6c6022
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2024-09-04 21:45:15 -0300

    UEG-23 Adicionadas as entidades de usuário, postagem e item de acervo

---
+++ src/main/java/br/ueg/acervodigital/arquitetura/model/User.java
@@ -0,0 +1,45 @@
+package br.ueg.acervodigital.arquitetura.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "person")
+public class User implements GenericModel<Long> {
+    public final static String SEQUENCE_NAME = "user_sequence";
+
+    @Id
+    @SequenceGenerator(
+            name=SEQUENCE_NAME,
+            sequenceName = SEQUENCE_NAME + "_bd",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = SEQUENCE_NAME
+    )
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name",  nullable = false)
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "function")
+    private String function;
+
+}
