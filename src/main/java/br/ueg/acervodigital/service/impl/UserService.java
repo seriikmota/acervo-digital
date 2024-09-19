921e49272a736954aecdb9bb96377d69abd9c7e6
Erik Mota <seriikmota.dev@gmail.com>
2024-09-19 11:53:05 -0300

    UEG-24 Configurado crud de usuário

--- src/main/java/br/ueg/acervodigital/service/impl/UserService.java
+++ src/main/java/br/ueg/acervodigital/service/impl/UserService.java
@@ -1,4 +1,31 @@
 package br.ueg.acervodigital.service.impl;
 
-public class UserService {
+import br.ueg.acervodigital.dto.list.UserListDTO;
+import br.ueg.acervodigital.dto.request.UserRequestDTO;
+import br.ueg.acervodigital.dto.response.UserResponseDTO;
+import br.ueg.acervodigital.entities.User;
+import br.ueg.acervodigital.mapper.UserMapper;
+import br.ueg.acervodigital.repository.UserRepository;
+import br.ueg.acervodigital.service.IUserService;
+import br.ueg.acervodigitalarquitetura.service.impl.AbstractCrudService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends AbstractCrudService<UserRequestDTO, UserResponseDTO, UserListDTO, User, UserRepository, UserMapper, Long>
+        implements IUserService {
+
+    @Override
+    protected void prepareToCreate(User data) {
+
+    }
+
+    @Override
+    protected void prepareToUpdate(User dataDB) {
+
+    }
+
+    @Override
+    protected void prepareToDelete(User dataDB) {
+
+    }
 }
