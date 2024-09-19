ae55fef9939206361a67368859f6c71cdd93dcc7
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2024-09-19 14:55:44 -0300

    UEG-28 Adicionado crud generico a arquitetura do segundo semestre.

---
+++ src/main/java/br/ueg/acervodigitalarquitetura/controller/GenericCRUDController.java
@@ -0,0 +1,157 @@
+package br.ueg.acervodigitalarquitetura.controller;
+
+import br.ueg.acervodigitalarquitetura.domain.BaseModel;
+import br.ueg.acervodigitalarquitetura.exception.MessageResponse;
+import br.ueg.acervodigitalarquitetura.mapper.GenericMapper;
+import br.ueg.acervodigitalarquitetura.service.CrudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class GenericCRUDController<
+        DTO,
+        DTOCreate,
+        DTOUpdate,
+        DTOList,
+        MODEL extends BaseModel<TYPE_PK>,
+        TYPE_PK,
+        SERVICE extends CrudService<
+                        MODEL,
+                        TYPE_PK>,
+        MAPPER extends GenericMapper<DTO,DTOCreate, DTOUpdate, DTOList , MODEL, TYPE_PK>
+        > {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    protected SERVICE service;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    protected MAPPER mapper;
+
+    @PreAuthorize(value = "hasRole(#root.this.getRoleName('CREATE'))")
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @Operation(description = "Método utilizado para realizar a inclusão de um entidade",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Entidade Incluida",
+                            useReturnTypeSchema = true),
+                    @ApiResponse(responseCode = "403", description = "Acesso negado",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = MessageResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Erro de Negócio",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = MessageResponse.class)))
+            })
+    public ResponseEntity<DTO> create(@RequestBody DTOCreate dto) {
+        MODEL inputModel = mapper.fromModelCreatedToModel(dto);
+        DTO resultDTO = mapper.toDTO(service.create(inputModel));
+        return ResponseEntity.ok(resultDTO);
+    }
+
+
+    @PreAuthorize(value = "hasRole(#root.this.getRoleName('UPDATE'))")
+    @PutMapping(path = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(description = "Método utilizado para altlerar os dados de uma entidiade", responses = {
+            @ApiResponse(responseCode = "200", description = "Listagem geral",
+                    useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Acesso negado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Erro de Negócio",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class)))
+    }
+    )
+    public ResponseEntity<DTO> update(
+            @RequestBody DTOUpdate dto,
+            @PathVariable("id") TYPE_PK id) {
+        MODEL inputModel = mapper.fromModelUpdatedToModel(dto);
+        inputModel.setId(id);
+        MODEL modelSaved = service.update(inputModel);
+        return ResponseEntity.ok(mapper.toDTO(modelSaved));
+    }
+
+    @PreAuthorize(value = "hasRole(#root.this.getRoleName('READ_ALL'))")
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(description = "lista todos modelos", responses = {
+            @ApiResponse(responseCode = "200", description = "Listagem geral",
+                    useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Acesso negado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Erro de Negócio",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class)))
+    })
+    public ResponseEntity<List<DTOList>> listAll() {
+        List<DTOList> modelList = mapper.fromModelToDTOList(service.listAll());
+        return ResponseEntity.of(
+                Optional.ofNullable(modelList)
+        );
+    }
+
+    @PreAuthorize(value = "hasRole(#root.this.getRoleName('READ'))")
+    @GetMapping(path = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(description = "Obter os dados completos de uma entidiade pelo id informado!", responses = {
+            @ApiResponse(responseCode = "200", description = "Entidade encontrada",
+                    useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Acesso negado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class)))
+    })
+    public ResponseEntity<DTO> getById(
+            @PathVariable("id") TYPE_PK id
+    ) {
+        DTO dtoResult = mapper.toDTO(service.getById(id));
+        return ResponseEntity.ok(dtoResult);
+    }
+
+    @PreAuthorize(value = "hasRole(#root.this.getRoleName('DELETE'))")
+    @DeleteMapping(path ="/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @Operation(description = "Método utilizado para remover uma entidiade pela id informado", responses = {
+            @ApiResponse(responseCode = "200", description = "Entidade Removida",
+                    useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "Registro não encontrado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "403", description = "Acesso negado",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = MessageResponse.class)))
+    })
+    public ResponseEntity<DTO> remove(
+            @PathVariable("id") TYPE_PK id
+    ) {
+        DTO dtoResult = mapper.toDTO(service.deleteById(id));
+        return ResponseEntity.ok(dtoResult);
+    }
+
+    public String getRoleName(String action){
+        return "ROLE_".concat(this.service.getEntityType().getSimpleName().toUpperCase().concat("_"+action.toUpperCase()));
+    }
+}
