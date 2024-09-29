56ffcc86352ae2145f8ba26464d2f86534c1f68a
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2024-09-29 13:43:56 -0300

    UEG-30-Adicionar-Filtragem-para-os-Itens-do-Acervo-BackEnd Adicionados os campos obrigatórios do item de acervo

---
+++ src/main/java/br/ueg/acervodigital/service/validations/item/ItemMandatoryFields.java
@@ -0,0 +1,27 @@
+package br.ueg.acervodigital.service.validations.item;
+
+import br.ueg.acervodigital.entities.Item;
+import br.ueg.acervodigital.entities.User;
+import br.ueg.acervodigital.exception.BusinessRuleException;
+import br.ueg.acervodigitalarquitetura.enums.ValidationActionsEnum;
+import br.ueg.acervodigitalarquitetura.validation.IValidations;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemMandatoryFields implements IValidations<Item> {
+    @Override
+    public void validate(Item data, ValidationActionsEnum action) {
+        if (data.getNumber_code() == null) {
+            throw new BusinessRuleException("O código do item é obrigatório!");
+        }
+        if (data.getName() == null) {
+            throw new BusinessRuleException("A descrição do item é obrigatória!");
+        }
+        if (data.getCollector() == null) {
+            throw new BusinessRuleException("O nome de quem coletou o item é obrigatório!");
+        }
+        if (data.getColleaction_year() == null) {
+            throw new BusinessRuleException("O ano que realizou-se a coleta do item é obrigatório!");
+        }
+    }
+}
