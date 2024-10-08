28794213fb7913e3ed128e17a562d71c80d88c04
Erik Mota <seriikmota.dev@gmail.com>
2024-10-07 23:25:40 -0300

    fix: corrigindo handler de acesso negado

--- src/main/java/br/ueg/acervodigital/exception/CustomHandlerException.java
+++ src/main/java/br/ueg/acervodigital/exception/CustomHandlerException.java
@@ -1,12 +1,19 @@
 package br.ueg.acervodigital.exception;
 
 import br.ueg.acervodigitalarquitetura.exception.ApiResponseExceptionHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomHandlerException extends ApiResponseExceptionHandler {
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<String> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
 
     @ExceptionHandler(BusinessRuleException.class)
     public ResponseEntity<String> handleBusinessRuleException(BusinessRuleException ex) {
