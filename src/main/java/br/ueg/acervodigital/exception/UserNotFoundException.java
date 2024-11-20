e52af87b873bd4dec9d560692989903ad53bb9db
Erik Mota <seriikmota.dev@gmail.com>
2024-11-20 13:18:40 -0300

    refactor: refatorando exceptions e padronizando retorno de erros

--- src/main/java/br/ueg/acervodigital/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.ueg.acervodigital.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
